2bbbd317f127c47defda37ffdd7d6aaedddb55da
Debrath Sharma <debrath.sharma@hazesoft.co>
2023-02-22 14:53:31 +0545

    question one html file completed

--- 2076/questionOne.php
+++ 2076/questionOne.php
@@ -1,23 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Question: 1</title>
-  </head>
-  <body>
-    <!-- 1.Create web form for book search catalog. The form should contain a dropdown defining search type, a text box for search keyword,
-a radio button for download type true or false, now write PHP script to store data from the form into database table and also retrive the
-results from stored table in a new page.[10] -->
+<?php
 
-    <div class="main-div">
-      <!-- Creating a form -->
-      <form action="post">
-        <?php
-        echo "I am php inside the form";
-        ?>
-      </form>
-    </div>
-  </body>
-</html>
